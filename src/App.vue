23325783c4c56bd9fe9cbd20068a113b91ce8f83
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-04-10 18:17:27 +0200

    add MainComponent and FooterTopComponent

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,27 @@
 <template>
-  
   <HeaderComponent />
+  <MainComponent />
+  <FooterTopComponent />
 </template>
 
 <script>
 import HeaderComponent from './components/HeaderComponent.vue';
+import MainComponent from './components/MainComponent.vue';
+import FooterTopComponent from './components/FooterTopComponent.vue';
   export default {
     name: 'App',
     components: {
-      HeaderComponent
+      HeaderComponent,
+      MainComponent,
+      FooterTopComponent,
+    },
+    data() {
+      return {
+
+      }
     }
   }
 </script>
 
 <style lang="scss" scoped>
-
 </style>
