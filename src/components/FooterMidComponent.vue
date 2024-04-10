b66f5eb3527ba90d97c15ba8ebea0cdaeda56c4f
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-04-10 18:36:23 +0200

    Created FooterMidComponent

---
+++ src/components/FooterMidComponent.vue
@@ -0,0 +1,41 @@
+<template>
+  <footer class="mid">
+    <div class="container">
+      <div class="left">
+        <ul>
+            <li>
+                
+            </li>
+        </ul>
+      </div>
+      <div class="right">
+
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "FooterMidComponent",
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.mid {
+  background-image: url(./public/img/footer-bg.jpg);
+}
+.right {
+  width: 50%;
+}
+.left {
+  background-image: url(./public/img/dc-logo-bg.png);
+  width: 50%;
+  img {
+    width: 100%;
+  }
+}
+</style>
