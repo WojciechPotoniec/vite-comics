f49205c5f50e71e4775d4cc124ec05cb1edb1c63
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-04-10 18:36:04 +0200

    add FooterMidComponent

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
   <HeaderComponent />
   <MainComponent />
   <FooterTopComponent />
+  <FooterMidComponent />
 </template>
 
 <script>
 import HeaderComponent from './components/HeaderComponent.vue';
 import MainComponent from './components/MainComponent.vue';
 import FooterTopComponent from './components/FooterTopComponent.vue';
+import FooterMidComponent from './components/FooterMidComponent.vue';
   export default {
     name: 'App',
     components: {
       HeaderComponent,
       MainComponent,
       FooterTopComponent,
+      FooterMidComponent,
     },
     data() {
       return {
