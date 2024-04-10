924428088ea585c726873a1cbefdc1028bda1e45
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-04-10 18:18:20 +0200

    created MainComponent

---
+++ src/components/MainComponent.vue
@@ -0,0 +1,24 @@
+<template>
+    <main>
+        <div class="container">
+            <h2 class="text-light">Content goes here</h2>
+        </div>
+    </main>
+</template>
+
+<script>
+    export default {
+        name: 'MainComponent',
+    }
+</script>
+
+<style lang="scss" scoped>
+@use "./src/assets/styles/partials/variables" as *;
+main{
+  background-color: $bg-color-main;
+  padding: 40px 0;
+}
+h2{
+    margin: 0;
+}
+</style>
