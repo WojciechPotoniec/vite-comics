e1b15badd6ab44c72271e26e7e434629eb2c1624
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-04-10 21:21:59 +0200

    update mainlists in data

--- src/components/FooterMidComponent.vue
+++ src/components/FooterMidComponent.vue
@@ -2,17 +2,16 @@
   <footer class="mid">
     <div class="container d-flex">
       <div class="left">
-        <ul class="text-light" v-for="(title, index) in titles" :key="index">
-          {{
-            title
-          }}
-          <li v-for="(item, index) in subtitles" :key="index">
-            {{ subtitles[index] }}
+        <ul class="text-light" v-for="(mainlist, index) in mainlists" :key="index">
+          <span>
+            {{mainlist.title}}
+          </span>
+          <li v-for="(item, i) in mainlist.list" :key="i">
+            <a href="#">{{ item }}</a>
           </li>
         </ul>
       </div>
       <div class="right">
-
       </div>
     </div>
   </footer>
@@ -24,16 +23,53 @@ export default {
   data() {
     return {
       img: "./public/img/dc-logo-bg.png",
-      titles: ["dc comics", "dc", "sites", "shop"],
-      subtitles: [
-        "Characters",
-        "Comics",
-        "Movies",
-        "TV",
-        "Games",
-        "Videos",
-        "News",
-      ],
+      mainlists: [
+                    {
+                        title: 'DC COMICS',
+                        list: [
+                          'Characters',
+                            'Comics',
+                            'Movies',
+                            'TV',
+                            'Games',
+                            'Videos',
+                            'News'
+                        ]
+                    },
+                    {
+                        title: 'DC',
+                        list: [
+                            'Terms Of Use',
+                            'Privacy policy (New)',
+                            'Ad Choices',
+                            'Advertising',
+                            'Jobs',
+                            'Subscriptions',
+                            'Talent Workshops',
+                            'CPSC Certificates',
+                            'Ratings',
+                            'Shop Help',
+                            'Contact Us'
+                        ]
+                    },
+                    {
+                        title: 'SITES',
+                        list: [
+                            'DC',
+                            'MAD Magazine',
+                            'DC Kids',
+                            'DC Universe',
+                            'DC Power Visa'
+                        ]
+                    },
+                    {
+                        title: 'SHOP',
+                        list: [
+                            'Shop DC',
+                            'Shop DC Collectibles'
+                        ]
+                    }
+                ],
     };
   },
 };
@@ -45,20 +81,27 @@ export default {
 }
 .left {
   display: flex;
-  width: 50%;
-  img {
-    width: 100%;
-  }
+  flex-wrap: wrap;
+  width: 40%;
 }
-ul {
+ul{
   list-style: none;
+}
+span{
   text-transform: uppercase;
-  li {
-    text-transform: lowercase;
+  font-weight: 900;
+}
+a{
+  text-decoration: none;
+  color: grey;
+  font-size: 0.9rem;
+  &:hover{
+    color: white;
   }
 }
+
 .right {
-  width: 50%;
+  width: 60%;
   background-image: url(./public/img/dc-logo-bg.png);
   background-repeat: no-repeat;
   background-position: center;
