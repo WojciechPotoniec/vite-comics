8a324d384e49ab4720924092ca0dc44307081562
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-04-10 16:39:09 +0200

    init HeaderComponent

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,20 +1,24 @@
 <template>
-   <header>
-        <h2>Header</h2>
-   </header>
+  <header class="container">
+    <img :src="img" alt="DC logo"/>
+    <h2>Header</h2>
+  </header>
 </template>
 
 <script>
-    export default {
-        name: 'HeaderComponent',
-        data() {
-            return {
-                
-            }
-        }
-    }
+export default {
+  name: "HeaderComponent",
+  data() {
+    return {
+        img: "../public/img/dc-logo.png"
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-
+@use "./src/assets/styles/partials/variables" as *;
+header {
+  background-color: $bg-color-header;
+}
 </style>
