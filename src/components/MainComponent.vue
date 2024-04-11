6e139ff07514188a73583362835866bef4f63c47
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-04-11 18:59:48 +0200

    update main templat. imported array of data from data.js. imported CardComponent. created some css rules in style

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,30 +1,78 @@
 <template>
-    <main>
-        <div class="container">
-            <h2 class="text-light">Content goes here</h2>
+  <div id="hero"></div>
+  <main>
+    <div class="container">
+      <h3>Current series</h3>
+      <div class="row">
+        <div
+          class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 mb-4"
+          v-for="(item, index) in comics"
+          :key="index"
+        >
+          <CardComponent
+            :image="item.thumb"
+            :title="item.series"
+            :text="item.type"
+          />
         </div>
-    </main>
+      </div>
+      <div class="load">
+        <button>Load More</button>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script>
-import {comics} from '../data/data.js'
-    export default {
-        name: 'MainComponent',
-        data(){
-            return{
-                comics: comics
-            }
-        }
-    }
+import { comics } from "../data/data.js";
+import CardComponent from "./CardComponent.vue";
+export default {
+  name: "MainComponent",
+  components: {
+    CardComponent,
+  },
+  data() {
+    return {
+      comics: comics,
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 @use "./src/assets/styles/partials/variables" as *;
-main{
+#hero {
+  background-image: url(../public/img/jumbotron.jpg);
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: 400px;
+}
+main {
   background-color: $bg-color-main;
   padding: 40px 0;
 }
-h2{
-    margin: 0;
+h2 {
+  margin: 0;
+}
+h3 {
+  background-color: $bg-color-footer-top;
+  display: inline-block;
+  transform: translate(0, -140%);
+  padding: 5px 20px;
+  color: white;
+  text-transform: uppercase;
+}
+.load{
+    @include wp-flex;
+    button{
+        background-color: $bg-color-footer-top;
+        padding: 6px 50px;
+        text-transform: uppercase;
+        color: white;
+        border: none;
+        font-size: small;
+        font-weight: 600;
+        cursor: pointer;
+    }
 }
 </style>
